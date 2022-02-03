702d8c2ba04d5c8247894df90b9ebd26b8107ec3
Peter <peter@trampoline.cx>
2022-02-03 14:16:57 -0500

    feat: no-keyword-prefix allow class

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -125,7 +125,7 @@ module.exports = {
     'unicorn/no-for-loop': 'error',
     'unicorn/no-instanceof-array': 'error',
     'unicorn/no-invalid-remove-event-listener': 'error',
-    'unicorn/no-keyword-prefix': 'error',
+    'unicorn/no-keyword-prefix': ['error', { disallowedPrefixes: ['new', 'for'] }],
     'unicorn/no-lonely-if': 'error',
     'unicorn/no-new-buffer': 'error',
     'unicorn/no-unreadable-array-destructuring': 'error',
